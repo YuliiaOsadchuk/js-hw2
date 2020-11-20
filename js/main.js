9572619ff1f4b14985ac2c50d8cc38c1942d4dcf
YuliiaOsadchuk <yulia.zelenskaya@gmail.com>
2020-11-20 11:07:47 +0200

    added conditions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-let firstNumber = +prompt("Введіть перше ціле число");
-let secondNumber = +prompt("Введіть друге ціле число");
-const isFirstNumber = Number.isInteger(firstNumber);
-const isSecondNumber = Number.isInteger(secondNumber);
+let firstNumber = 0;
+do {
+    firstNumber = +prompt("Введіть перше ціле число");
+} while (!(Number.isInteger(firstNumber)) || firstNumber === NaN);
+
+let secondNumber = 0;
+do {
+    secondNumber = +prompt("Введіть друге ціле число");
+} while (!(Number.isInteger(secondNumber)) || secondNumber === NaN)
 
 const isCountEven = confirm("Рахувати парні числа?");
 
 let sum = 0;
 for (let i = firstNumber; i <= secondNumber; i++) {
-    if (!isFirstNumber || !isSecondNumber) break;
     if (!isCountEven && (i % 2 === 0)) continue;
     sum += i;
 }
